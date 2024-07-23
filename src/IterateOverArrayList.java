dd94a1a78175ab1414ad299d7ce966b760694ec2
Rikimo23 <162639270+Rikimo23@users.noreply.github.com>
2024-07-23 15:51:25 -0400

    commit

---
+++ src/IterateOverArrayList.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class IterateOverArrayList {
+    public static void main(String[] args) {
+        List<String> tvShows = new ArrayList<>();
+        tvShows.add("Breaking Bad");
+        tvShows.add("Game Of Thrones");
+        tvShows.add("Friends");
+        tvShows.add("Prison break");
+        System.out.println("\n=== Iterate using an iterator() ===");
+        Iterator<String> tvShowIterator = tvShows.iterator();
+        while (tvShowIterator.hasNext()) {
+            String tvShow = tvShowIterator.next();
+            System.out.println(tvShow);
+        }
+
+        System.out.println("==Iterate using an iterator() and forEachRemaining() method ===");
+        tvShowIterator = tvShows.iterator();
+        tvShowIterator.forEachRemaining(tvShow -> {
+            System.out.println(tvShow);
+        });
+
+
+        System.out.println("\n=== Iterate using simple for-each loop ===");
+        for (String tvShow : tvShows) {
+            System.out.println(tvShow);
+        }
+
+        System.out.println("\n=== Iterate using for loop with index ===");
+    }
+git 
